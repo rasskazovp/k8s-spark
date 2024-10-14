f800f4202852dc63a0593150ad24871457340162
Pavlo Rasskazov <Pavlo.Rasskazov@x-insight.pl>
2024-10-14 08:57:02 +0200

    tune parameters

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,19 +1,19 @@
 variable "resource_group_location" {
   type        = string
-  default     = "westeurope"
+  default     = "northeurope"
   description = "Location of the resource group."
 }
 
 variable "resource_group_name_prefix" {
   type        = string
-  default     = "rg"
+  default     = "rg-aks"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
 variable "node_count" {
   type        = number
   description = "The initial quantity of nodes for the node pool."
-  default     = 2
+  default     = 3
 }
 
 variable "msi_id" {
