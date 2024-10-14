7529e913c33df37c932aedc67bbb071b8a03e2c1
Pavlo Rasskazov <Pavlo.Rasskazov@x-insight.pl>
2024-10-14 08:57:28 +0200

    tune parameters

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -28,7 +28,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     name       = "agentpool"
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_D4as_v5"
     node_count = var.node_count
   }
   linux_profile {
